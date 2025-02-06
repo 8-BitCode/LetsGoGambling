98a371fdf9b152dd674e583b2919248bc32d9f3f
Khalid <khalid.albugamy@gmail.com>
2025-02-06 18:36:08 +0000

    added most basic logic

--- src/Slots.jsx
+++ src/Slots.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 //Helmet is used to give each sub page a title dynamically (just a little akram detail)
 import { Helmet } from 'react-helmet';
@@ -6,8 +6,29 @@ import { Helmet } from 'react-helmet';
 
 
 const Slots = () => {
-    var text = 0
-    var num1 = Math.floor(Math.random() * 10);
+    const [num1, setNum1] = useState(0);
+    const [num2, setNum2] = useState(0);
+    const [num3, setNum3] = useState(0);
+    const [message, setMessage] = useState('');
+  
+    function Ran_Num() {
+        const newNum1 = Math.floor(Math.random() * 4);
+        const newNum2 = Math.floor(Math.random() * 4);
+        const newNum3 = Math.floor(Math.random() * 4);
+      
+    // Check if all three numbers are equal
+      if (newNum1 === newNum2 && newNum2 === newNum3) {
+        setMessage('Wow! Good job buddy :)    +32 gambucks');
+      } else {
+        setMessage('LOSER!!! -1000 GAMBUCKS');
+      }
+  
+      // Update the state with the new numbers
+      setNum1(newNum1);
+      setNum2(newNum2);
+      setNum3(newNum3);
+    }
+
 
     return(
         <>
@@ -15,8 +36,15 @@ const Slots = () => {
         <Helmet>
             <title>SLOTS!!!!!</title>
         </Helmet>
+        <div className='Slots-Container'>
+            <div>Welcome to SLOTS!!</div>
+            <br></br>
+            <div>slots result: {num1}, {num2}, {num3}</div>
+            <div>{message}</div>
+            <button onClick={Ran_Num}> SPIN </button>
+            
+        </div>
 
-        <div>Slots Page</div>
         
         </>
     );
