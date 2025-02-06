87877b23cd69f34bae4b996b479d9bf7198e428e
Khalid <khalid.albugamy@gmail.com>
2025-02-06 17:56:45 +0000

    set up random number generator

--- src/Slots.jsx
+++ src/Slots.jsx
@@ -1,24 +1,32 @@
 import React from 'react';
+import {useState} from 'react'; 
 
 //Helmet is used to give each sub page a title dynamically (just a little akram detail)
 import { Helmet } from 'react-helmet';
 
 
 
-
-
 const Slots = () => {
+    var text = 0
+    var num1 = Math.floor(Math.random() * 10);
+
     return(
         <>
 
         <Helmet>
             <title>SLOTS!!!!!</title>
         </Helmet>
-
-        <div>Slots Page</div>
-        
+        <div className='Slots-Container'>
+            <div>WELCOME TO SLOTS</div>
+            <div>{text}</div>
+            <div>{num1}</div>
+            <button>hello</button>
+        </div>
         </>
     );
 }
 
+
+
 export default Slots;
+
