77832aa5493b61999912c0b1c71dcd56c668c29f
y31164ok <oliver.kot@student.manchester.ac.uk>
2025-02-07 18:08:25 +0000

    added chips with ability to assign different values to each

--- src/Roulette.jsx
+++ src/Roulette.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
+import Chip from './RouletteComponents/Chip';
+import { Fragment } from 'react';
 
 //Helmet is used to give each sub page a title dynamically (just a little akram detail)
 import { Helmet } from 'react-helmet';
 import './CssFiles/Roulette.css';
 
 const Roulette = () => {
+
+    const chipValues = [
+        0.5, 1, 5, 10, 20, 50, 100
+    ]
+
     return(
-        <>
+        <Fragment>
 
-        <Helmet>
-            <title>ROULETTE!!!!!</title>
-        </Helmet>
-
-        <div className='Roulette.css'>
-            <div>Roulette Page</div>
-        </div>
-
-        </>
+            <Helmet>
+                <title>ROULETTE!!!!!</title>
+            </Helmet>
+            <ul>
+            {chipValues.map(value => <li><Chip class={value} value={value} /></li>)}
+            </ul>
+    
+        </Fragment>
     );
 }
 
---
+++ src/RouletteComponents/Chip.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Chip({value}) {
+    return (<div className='Chip'>{value}</div>);
+}
+
+export default Chip;
