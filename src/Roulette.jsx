e1933f00ccae42fe298177b117ab4c7af81fc60a
y31164ok <oliver.kot@student.manchester.ac.uk>
2025-02-07 19:04:29 +0000

    chips return to starting position after being moved

--- src/Roulette.jsx
+++ src/Roulette.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Chip from './RouletteComponents/Chip';
 import { Fragment } from 'react';
 
+
 //Helmet is used to give each sub page a title dynamically (just a little akram detail)
 import { Helmet } from 'react-helmet';
 import './CssFiles/Roulette.css';
@@ -9,19 +10,19 @@ import './CssFiles/Roulette.css';
 const Roulette = () => {
 
     const chipValues = [
-        0.5, 1, 5, 10, 20, 50, 100
+        0.5, 1, 2, 5, 10, 20, 50, 100
     ]
 
     return(
         <Fragment>
-
+        <div class='Roulette-Container'>
             <Helmet>
                 <title>ROULETTE!!!!!</title>
             </Helmet>
-            <ul>
-            {chipValues.map(value => <li><Chip class={value} value={value} /></li>)}
-            </ul>
-    
+            
+            {chipValues.map(value => <Chip class={value} value={value} />)}
+            
+            </div>
         </Fragment>
     );
 }
--- src/RouletteComponents/Chip.jsx
+++ src/RouletteComponents/Chip.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import Draggable from 'react-draggable';
 
 function Chip({value}) {
-    return (<div className='Chip'>{value}</div>);
+    
+    let startingPositions = {
+        '0.5': {'x': 20, 'y': 200},
+        '1': {'x': 120, 'y': 200},
+        '2': {'x': 220, 'y': 200},
+        '5': {'x': 320, 'y': 200},
+        '10': {'x': 420, 'y': 200},
+        '20': {'x': 520, 'y': 200},
+        '50': {'x': 620, 'y': 200},
+        '100': {'x': 720, 'y': 200},
+    }
+
+    const startingX = startingPositions[value]['x']
+    const startingY = startingPositions[value]['y']
+
+    return (
+    <Draggable onMouseDown={returnPosition()} position = {{x: startingX, y: startingY}}>    
+        <div className='Chip'>{value}</div>
+    </Draggable>);
+}
+
+const returnPosition = () => {
+
 }
 
 export default Chip;
