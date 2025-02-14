31a3b85ab0b4d3134b1e486e5496099dfe8cf0f0
Akram! <akram.munirawel@student.manchester.ac.uk>
2025-02-14 19:46:43 +0000

    added database connections to blackjack

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore, doc, setDoc, getDoc, collection, query, where, getDocs } from "firebase/firestore";
+import { 
+  getFirestore, 
+  doc, 
+  setDoc, 
+  getDoc, 
+  updateDoc,  // ✅ Import updateDoc
+  collection, 
+  query, 
+  where, 
+  getDocs 
+} from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAPXMVVVHUtadmEPvXJLk4Mr1tPeOBK2cI",
@@ -10,7 +20,7 @@ const firebaseConfig = {
     messagingSenderId: "351332924647",
     appId: "1:351332924647:web:53717055ce33f75ca2b642",
     measurementId: "G-KP9034ETJC"
-  };
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -20,4 +30,4 @@ const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 const db = getFirestore(app);
 
-export { auth, provider, db, doc, setDoc, getDoc, collection, query, where, getDocs };
+export { auth, provider, db, doc, setDoc, getDoc, updateDoc, collection, query, where, getDocs };
