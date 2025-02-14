c8425c88299a5dac1c609ab4b75203f2b95d4978
y31164ok <oliver.kot@student.manchester.ac.uk>
2025-02-14 07:17:35 +0000

    completely scrapping intial idea, selectable chip changing bets, created the templates for the different kinds of bets you can place

--- src/Roulette.jsx
+++ src/Roulette.jsx
@@ -1,28 +1,54 @@
-import React from 'react';
-import Chip from './RouletteComponents/Chip';
-import { Fragment } from 'react';
+import React, { useState, Fragment } from 'react';
 
 
 //Helmet is used to give each sub page a title dynamically (just a little akram detail)
 import { Helmet } from 'react-helmet';
-import './CssFiles/Roulette.css';
+import './RouletteComponents/Roulette.css'
 
 const Roulette = () => {
 
-    const chipValues = [
-        0.5, 1, 2, 5, 10, 20, 50, 100
-    ]
+    const [chipSelected, setChip] = useState();
+
+    function updateSelected(value) {
+        setChip(value)
+    }
+
+    const chipValues = [0.5, 1, 2, 5, 10, 25, 50, 100, 500]; 
 
     return(
         <Fragment>
-        <div class='Roulette-Container'>
+        <div className='Roulette-Container'>
             <Helmet>
                 <title>ROULETTE!!!!!</title>
             </Helmet>
-            
-            {chipValues.map(value => <Chip class={value} value={value} />)}
-            
+
+            <div>Selected Chip: {chipSelected}</div>
+
+            <div id='chip-container'>        
+                {chipValues.map(value => <button onClick={() => updateSelected(value)} id={value} key={value} className='chip'>{value}</button>)}
             </div>
+
+            <div id='roulette-table'>
+                <div id='zero-column'>
+                    
+                </div>
+                <div id='row-one'>
+
+                </div>
+                <div id='row-two'>
+
+                </div>
+                <div id='row-three'>
+
+                </div>
+                <div id='row-four'>
+
+                </div>
+                <div id='row-five'>
+                    </div>            
+            </div>
+
+        </div>
         </Fragment>
     );
 }
---
+++ src/RouletteComponents/IntervalBet.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function ColourBet(colour) {
+
+ return(
+    <button>{colour}</button>
+ )   
+};
+
+export default ColourBet;
