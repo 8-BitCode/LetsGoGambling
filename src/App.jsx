4e537664fde69082d15d0bf7a3bdae99694d926d
Akram! <akram.munirawel@student.manchester.ac.uk>
2025-02-16 14:28:21 +0000

    Added boarder themed Border to title page-KEY!!!

--- src/App.jsx
+++ src/App.jsx
@@ -10,20 +10,35 @@ import Roulette from './Roulette';
 import Stats from './Stats';
 
 import './CssFiles/App.css';
+
 export default () => (
   <>
-  <div className='Intro-Container'>
-     <BrowserRouter>
-      <Routes>
-        <Route path="/TermsOfService" element={<TermsOfService />}/>
-        <Route path="/UserEntry" element={<UserEntry />} />
-        <Route path="/GameSelection" element={<GameSelection />} />
-        <Route path="/Blackjack" element={<Blackjack />} />
-        <Route path="/Slots" element={<Slots />} />
-        <Route path="/Roulette" element={<Roulette />} />
-        <Route path="/Stats" element={<Stats />} />
-      </Routes>
-    </BrowserRouter>
-  </div>
+    <div className='Windows95-Theme'>
+      {/* Title Bar (Now Part of Windows95-Theme) */}
+      <div className="Window-Title-Bar">
+        <span className="Window-Title">Hello.exe</span>
+        <div className="ButtonCon">
+          <button>-</button>
+          <button>X</button>
+        </div>
+      </div>
+
+      {/* Content Wrapper (Starts Right After Title Bar) */}
+      <div className='Content-Wrapper'>
+        <div className='Intro-Container'>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/TermsOfService" element={<TermsOfService />} />
+              <Route path="/UserEntry" element={<UserEntry />} />
+              <Route path="/GameSelection" element={<GameSelection />} />
+              <Route path="/Blackjack" element={<Blackjack />} />
+              <Route path="/Slots" element={<Slots />} />
+              <Route path="/Roulette" element={<Roulette />} />
+              <Route path="/Stats" element={<Stats />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </div>
+    </div>
   </>
 );
