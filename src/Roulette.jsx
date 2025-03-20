648258229072580a0b0510b756e2cb1571b27ef1
0liverkot <oliverkot19@gmail.com>
2025-03-20 16:42:09 +0000

    i think i just removed an array that wasnt used

--- src/Roulette.jsx
+++ src/Roulette.jsx
@@ -396,12 +396,6 @@ const Roulette = ({ closeGame, setLevel, updateLevelInFirestore }) => {
     ];
     
 
-    // all potention outcomes from wheel spin 
-    const possibleOutcomes = ['single', 'double', 1, 2, 3, 4, 5, 6, 7, 
-                              8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 
-                              19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-                              30, 31, 32, 33, 34, 35, 36];
-
     return(
         <Fragment>
         <div className='Roulette-Container'>
